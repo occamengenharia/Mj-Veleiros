fddfc03f756c6b15cbbc8703e8d6ee5cba36c931
Wesley Catuzzo <62621841+WesleyCatuzzo@users.noreply.github.com>
2022-09-30 10:51:10 -0300

    Initial commit

---
+++ app/public/wp-content/themes/wp-devs/index.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+        <!-- -->
+        <div id="content" class="site">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero">
+                        Hero
+                    </section>
+                    <section class="services">
+                        Services
+                    </section>
+                    <section class="home-blog">
+                        <?php 
+                        ?>
+                    </section>
+                </main>
+            </div>
+        </div>
+        <!-- -->
+<?php get_footer(); ?>
